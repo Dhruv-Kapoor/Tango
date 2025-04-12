bbf64b413c659ecfb3301d7fb743601e0a68ecac
Dhruv Kapoor <dhruvkapoor.200@gmail.com>
2025-04-12 18:44:14 +0530

    test

---
+++ update_firestore.js
@@ -0,0 +1,20 @@
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore, FieldValue } = require('firebase-admin/firestore');
+const serviceAccount = JSON.parse(process.env.$FIREBASE_SERVICE_ACCOUNT);
+
+initializeApp({
+credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+async function updateRelease() {
+await db.collection('config').doc('config').set({
+  latestVersionCode: parseInt(process.env.VERSION_CODE),
+  updatedOn: FieldValue.serverTimestamp()
+}, { merge: true });
+
+console.log('Firestore document updated successfully');
+}
+
+updateRelease().catch(console.error);
