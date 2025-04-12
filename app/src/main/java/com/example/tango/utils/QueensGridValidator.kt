2c36b7e53b7b3433700d5352be821a207ac77660
Dhruv Kapoor <dhruvkapoor.200@gmail.com>
2025-04-12 15:22:02 +0530

    fixed queens validator

--- app/src/main/java/com/example/tango/utils/QueensGridValidator.kt
+++ app/src/main/java/com/example/tango/utils/QueensGridValidator.kt
@@ -91,144 +91,18 @@ fun validateQueensGrid(grid: Array<Array<QueensCellData>>, i: Int, j: Int): Bool
     return !blanks && invalidCells.isEmpty() && invalidRows.isEmpty() && invalidCols.isEmpty()
 }
 
-fun autooPlaceX(grid: Array<Array<QueensCellData>>, placedI: Int, placedJ: Int) {
-    val toCheck = QueensCellValue.BLANK
-    val newVal = QueensCellValue.CROSS
-    val autoX = true
-    if (grid[placedI][placedJ].value != QueensCellValue.QUEEN) {
-        return
-    }
-    for (i in grid.indices) {
-        if (grid[i][placedJ].value == toCheck) {
-            grid[i][placedJ].value = newVal
-            grid[i][placedJ].autoX = autoX
-        }
-    }
-    for (j in grid[placedI].indices) {
-        if (grid[placedI][j].value == toCheck) {
-            grid[placedI][j].value = newVal
-            grid[placedI][j].autoX = autoX
-        }
-    }
-
-    if (placedI > 0) {
-        if (placedJ > 0 && grid[placedI - 1][placedJ - 1].value == toCheck) {
-            grid[placedI - 1][placedJ - 1].value = newVal
-            grid[placedI - 1][placedJ - 1].autoX = autoX
-        }
-        if (placedJ < grid[0].size - 1 && grid[placedI - 1][placedJ + 1].value == toCheck) {
-            grid[placedI - 1][placedJ + 1].value = newVal
-            grid[placedI - 1][placedJ + 1].autoX = autoX
-        }
-    }
-    if (placedI < grid.size - 1) {
-        if (placedJ > 0 && grid[placedI + 1][placedJ - 1].value == toCheck) {
-            grid[placedI + 1][placedJ - 1].value = newVal
-            grid[placedI + 1][placedJ - 1].autoX = autoX
-        }
-        if (placedJ < grid[0].size - 1 && grid[placedI + 1][placedJ + 1].value == toCheck) {
-            grid[placedI + 1][placedJ + 1].value = newVal
-            grid[placedI + 1][placedJ + 1].autoX = autoX
-        }
-    }
-
-
-    val visited = hashSetOf<Pair<Int, Int>>()
-    fun dfs(i: Int, j: Int, color: Int) {
-        if (i < 0 || j < 0 || i >= grid.size || j >= grid[0].size || grid[i][j].color != color ||
-            Pair(i, j) in visited
-        ) {
-            return
-        }
-        visited.add(Pair(i, j))
-        if (grid[i][j].value == toCheck) {
-            grid[i][j].value = newVal
-            grid[i][j].autoX = autoX
-        }
-        dfs(i + 1, j, color)
-        dfs(i, j + 1, color)
-        dfs(i - 1, j, color)
-        dfs(i, j - 1, color)
-    }
-
-    dfs(placedI, placedJ, grid[placedI][placedJ].color)
-}
-
-fun autoRemoveX(grid: Array<Array<QueensCellData>>, placedI: Int, placedJ: Int) {
-    val toCheck = QueensCellValue.CROSS
-    val newVal = QueensCellValue.BLANK
-    val autoX = false
-    if (grid[placedI][placedJ].value != QueensCellValue.QUEEN) {
-        return
-    }
-    for (i in grid.indices) {
-        if (grid[i][placedJ].value == toCheck && grid[i][placedJ].autoX) {
-            grid[i][placedJ].value = newVal
-            grid[i][placedJ].autoX = autoX
-        }
-    }
-    for (j in grid[placedI].indices) {
-        if (grid[placedI][j].value == toCheck && grid[placedI][j].autoX) {
-            grid[placedI][j].value = newVal
-            grid[placedI][j].autoX = autoX
-        }
-    }
-
-    if (placedI > 0) {
-        if (placedJ > 0 && grid[placedI - 1][placedJ - 1].value == toCheck && grid[placedI - 1][placedJ - 1].autoX) {
-            grid[placedI - 1][placedJ - 1].value = newVal
-            grid[placedI - 1][placedJ - 1].autoX = autoX
-        }
-        if (placedJ < grid[0].size - 1 && grid[placedI - 1][placedJ + 1].value == toCheck && grid[placedI - 1][placedJ + 1].autoX) {
-            grid[placedI - 1][placedJ + 1].value = newVal
-            grid[placedI - 1][placedJ + 1].autoX = autoX
-        }
-    }
-    if (placedI < grid.size - 1) {
-        if (placedJ > 0 && grid[placedI + 1][placedJ - 1].value == toCheck && grid[placedI + 1][placedJ - 1].autoX) {
-            grid[placedI + 1][placedJ - 1].value = newVal
-            grid[placedI + 1][placedJ - 1].autoX = autoX
-        }
-        if (placedJ < grid[0].size - 1 && grid[placedI + 1][placedJ + 1].value == toCheck && grid[placedI + 1][placedJ + 1].autoX) {
-            grid[placedI + 1][placedJ + 1].value = newVal
-            grid[placedI + 1][placedJ + 1].autoX = autoX
-        }
-    }
-
-
-    val visited = hashSetOf<Pair<Int, Int>>()
-    fun dfs(i: Int, j: Int, color: Int) {
-        if (i < 0 || j < 0 || i >= grid.size || j >= grid[0].size || grid[i][j].color != color ||
-            Pair(i, j) in visited
-        ) {
-            return
-        }
-        visited.add(Pair(i, j))
-        if (grid[i][j].value == toCheck && grid[i][j].autoX) {
-            grid[i][j].value = newVal
-            grid[i][j].autoX = autoX
-        }
-        dfs(i + 1, j, color)
-        dfs(i, j + 1, color)
-        dfs(i - 1, j, color)
-        dfs(i, j - 1, color)
-    }
-
-    dfs(placedI, placedJ, grid[placedI][placedJ].color)
-}
-
-
 fun autoPlaceX(grid: Array<Array<QueensCellData>>, placedI: Int, placedJ: Int) {
-    val orgValue = grid[placedI][placedJ].value
     val queens = hashSetOf<Pair<Int, Int>>()
     val queenRows = hashSetOf<Int>()
     val queenCols = hashSetOf<Int>()
+    val colors = hashSetOf<Int>()
     for (i in grid.indices) {
         for (j in grid[i].indices) {
             if (grid[i][j].value == QueensCellValue.QUEEN) {
                 queens.add(Pair(i, j))
                 queenRows.add(i)
                 queenCols.add(j)
+                colors.add(grid[i][j].color)
             } else if (grid[i][j].value == QueensCellValue.CROSS && grid[i][j].autoX) {
                 grid[i][j].value = QueensCellValue.BLANK
             }
@@ -242,7 +116,8 @@ fun autoPlaceX(grid: Array<Array<QueensCellData>>, placedI: Int, placedJ: Int) {
                                 Pair(i - 1, j - 1) in queens ||
                                 Pair(i - 1, j + 1) in queens ||
                                 Pair(i + 1, j - 1) in queens ||
-                                Pair(i + 1, j + 1) in queens
+                                Pair(i + 1, j + 1) in queens ||
+                                grid[i][j].color in colors
                         )
             ) {
                 grid[i][j].value = QueensCellValue.CROSS
