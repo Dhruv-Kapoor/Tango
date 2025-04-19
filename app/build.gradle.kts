5550867b461a66d9cedc8a7f63502bd2b2bb0135
Dhruv Kapoor <dhruvkapoor.200@gmail.com>
2025-04-20 00:24:06 +0530

    added build step in workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,10 +30,18 @@ android {
             value = properties.getProperty("SERVER_CLIENT_ID")
         )
     }
-
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "release.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -50,12 +58,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-}
-
-afterEvaluate {
-    tasks.named("packageDebug") {
-        finalizedBy("copyBuildFiles")
     }
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,3 @@ plugins {
     alias(libs.plugins.google.services) apply false
     id("com.google.firebase.crashlytics") version "3.0.3" apply false
 }
-
-subprojects {
-    tasks.register<Copy>("copyBuildFiles") {
-        from(layout.buildDirectory.dir("outputs/apk/debug")) {
-            include("*.apk")
-        }
-        rename {
-            "debug.apk"
-        }
-        into(layout.projectDirectory.dir("../builds"))
-    }
-}
