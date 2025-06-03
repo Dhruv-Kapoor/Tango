c17d753d541f072a0f67c17e7276b7c5b130c891
Dhruv Kapoor <dhruvkapoor.200@gmail.com>
2025-06-03 18:07:25 +0530

    updated versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.tango"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "2.0.0"
+        versionCode = 28
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
